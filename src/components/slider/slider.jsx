13adc872b2855e628e3061169eeea62493ddfa99
frkam <gearboxop@gmail.com>
2022-06-23 18:37:33 +0300

    added header and slider components

---
+++ src/components/slider/slider.jsx
@@ -0,0 +1,69 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+import "../../styles/swiper.scss";
+import styles from "./slider.module.scss";
+import sliderImage from "../../images/sliderImage.png";
+
+export const Slider = () => {
+  return (
+    <div className={styles.slider}>
+      <Swiper
+        modules={[Pagination]}
+        pagination={{ clickable: true }}
+        className={styles.slider__swiper}
+        loop={true}
+      >
+        <SwiperSlide>
+          <div className={styles.slider__slide}>
+            <div className={styles.slide__text}>
+              <h1 className={styles.text__header}>
+                Как бизнесу сохранять <br />
+                IT-кадры на фоне кризиса
+              </h1>
+              <p className={styles.text__paragraph}>
+                Инструменты, которые могут использовать компании <br />
+                для удержания сотрудников
+              </p>
+              <button className={styles.text__button}>Подробнее</button>
+            </div>
+            <img src={sliderImage} alt="User" className={styles.slide__image} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.slider__slide}>
+            <div className={styles.slide__text}>
+              <h1 className={styles.text__header}>
+                Как бизнесу сохранять <br />
+                IT-кадры на фоне кризиса
+              </h1>
+              <p className={styles.text__paragraph}>
+                Инструменты, которые могут использовать компании <br />
+                для удержания сотрудников
+              </p>
+              <button className={styles.text__button}>Подробнее</button>
+            </div>
+            <img src={sliderImage} alt="User" className={styles.slide__image} />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.slider__slide}>
+            <div className={styles.slide__text}>
+              <h1 className={styles.text__header}>
+                Как бизнесу сохранять <br />
+                IT-кадры на фоне кризиса
+              </h1>
+              <p className={styles.text__paragraph}>
+                Инструменты, которые могут использовать компании <br />
+                для удержания сотрудников
+              </p>
+              <button className={styles.text__button}>Подробнее</button>
+            </div>
+            <img src={sliderImage} alt="User" className={styles.slide__image} />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+};
---
+++ src/widgets/header/header.jsx
@@ -0,0 +1,41 @@
+import logo from "../../images/logo.svg";
+import styles from "./header.module.scss";
+import mail from "../../images/mail.svg";
+import phone from "../../images/phone.svg";
+import { useWindowWidth } from "../../hooks/useWindowWidth";
+import { BREAKPOINT_MOBILE_L } from "../../constants/breakpoints";
+
+export const Header = () => {
+  const width = useWindowWidth();
+
+  return (
+    <header className={styles.header}>
+      <div className={styles.header__container}>
+        <a href="#">
+          <img src={logo} alt="logo" className={styles.header__logo} />
+        </a>
+        <div className={styles.header__contacts}>
+          {width > BREAKPOINT_MOBILE_L ? (
+            <>
+              <a href="tel:88000000000" className={styles.contacts__tel}>
+                8 800 000 00 00
+              </a>
+              <a href="mailto:sales@logo.ru" className={styles.contacts__mail}>
+                sales@logo.ru
+              </a>
+            </>
+          ) : (
+            <>
+              <a href="tel:88000000000" className={styles.contacts__tel}>
+                <img src={phone} alt="phone" />
+              </a>
+              <a href="mailto:sales@logo.ru" className={styles.contacts__mail}>
+                <img src={mail} alt="mail" />
+              </a>
+            </>
+          )}
+        </div>
+      </div>
+    </header>
+  );
+};
