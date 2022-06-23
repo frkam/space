788d316022cebb350af6f4aa7d71103ebf501eeb
frkam <gearboxop@gmail.com>
2022-06-23 22:49:22 +0300

    fixed slider. added cards filter by author

--- src/components/atricles/atricles.jsx
+++ src/components/atricles/atricles.jsx
@@ -1,51 +1,133 @@
 import { useEffect, useState } from "react";
 import { ARTICLES_API } from "../../constants/api";
 import styles from "./article.module.scss";
+import user from "../../images/user.svg";
+import dropdown from "../../images/dropdown.svg";
 
 export const Articles = () => {
-  const [articlesData, setArticlesData] = useState(null);
+  const [data, setData] = useState(null);
+  const [isAuthorFilterOpened, setIsAuthorFilterOpened] = useState(false);
+  const [authorFilter, setAuthorFilter] = useState(null);
 
   useEffect(() => {
     const getArticlesData = async () => {
       const resp = await fetch(ARTICLES_API);
       const data = await resp.json();
-      setArticlesData(data);
+      setData(data);
     };
     getArticlesData();
   }, []);
 
-  console.log(articlesData);
+  const articlesData = data?.articles?.filter(
+    (article) => article.author === authorFilter
+  );
+
+  const authorsData = Array.from(
+    new Set(
+      data?.articles
+        ?.filter((article) => article.author)
+        .map((article) => article.author)
+    )
+  );
 
   return (
     <section className={styles.articles}>
       <div className={styles.articles__filters}>
-        <div>Dropdown</div>
+        <div className={styles.filters__author}>
+          <div className={styles.author__author}>
+            <img
+              src={user}
+              alt="User"
+              className={styles["author__user-image"]}
+            />
+            <span className={styles.author__name}>
+              {authorFilter ? authorFilter : "Выбор автора"}
+            </span>
+          </div>
+          <button
+            className={styles.author__dropdown}
+            onClick={() => setIsAuthorFilterOpened((prev) => !prev)}
+          >
+            <img src={dropdown} alt="dropdown" />
+          </button>
+          <div className={styles.author__authors}>
+            {isAuthorFilterOpened &&
+              authorsData.map((author) => {
+                return (
+                  <button
+                    key={author}
+                    className={`${styles.authors__author} ${
+                      author === authorFilter
+                        ? styles["authors__author--current"]
+                        : ""
+                    }`}
+                    onClick={() => setAuthorFilter(author)}
+                  >
+                    {author}
+                  </button>
+                );
+              })}
+          </div>
+        </div>
         <div>От и до</div>
       </div>
       <div className={styles.articles__container}>
-        {articlesData?.articles?.map((article) => {
-          let date = new Date(Date.parse(article?.publishedAt))
-            .toLocaleDateString("ru-RU", {
-              day: "numeric",
-              month: "long",
-              year: "numeric",
-            })
-            .replace(".", "")
-            .replace(" г", "");
+        {authorFilter &&
+          articlesData?.map((article) => {
+            const date = new Date(Date.parse(article?.publishedAt))
+              .toLocaleDateString("ru-RU", {
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+              })
+              .replace(".", "")
+              .replace(" г", "");
 
-          return (
-            <div key={article.content} className={styles.articles__article}>
-              <span className={styles.article__date}>{date}</span>
-              <h1 className={styles.article__title}>{article?.title}</h1>
-              <p className={styles.article__paragraph}>
-                {article?.description}
-              </p>
-              {article.author && (
-                <div className={styles.article__author}>{article?.author}</div>
-              )}
-            </div>
-          );
-        })}
+            const title = article?.title.replace(/\uFFFD/g, "");
+
+            const paragraph = article?.description.replace(/\uFFFD/g, "");
+
+            return (
+              <div key={article.content} className={styles.articles__article}>
+                <span className={styles.article__date}>{date}</span>
+                <h1 className={styles.article__title}>{title}</h1>
+                <p className={styles.article__paragraph}>{paragraph}</p>
+                {article.author && (
+                  <div className={styles.article__author}>
+                    {article?.author}
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        {!authorFilter &&
+          data?.articles?.map((article) => {
+            const date = new Date(Date.parse(article?.publishedAt))
+              .toLocaleDateString("ru-RU", {
+                day: "numeric",
+                month: "long",
+                year: "numeric",
+              })
+              .replace(".", "")
+              .replace(" г", "");
+
+            const title = article?.title.replace(/\uFFFD/g, "");
+
+            const paragraph = article?.description.replace(/\uFFFD/g, "");
+
+            return (
+              <div key={article.content} className={styles.articles__article}>
+                <span className={styles.article__date}>{date}</span>
+                <h1 className={styles.article__title}>{title}</h1>
+                <p className={styles.article__paragraph}>{paragraph}</p>
+                {article.author && (
+                  <div className={styles.article__author}>
+                    {article?.author}
+                  </div>
+                )}
+              </div>
+            );
+          })}
       </div>
     </section>
   );
--- src/components/slider/slider.jsx
+++ src/components/slider/slider.jsx
@@ -1,69 +1,88 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper";
-import "swiper/css";
-import "swiper/css/pagination";
+import { useRef } from "react";
 import "../../styles/swiper.scss";
 import styles from "./slider.module.scss";
 import sliderImage from "../../images/sliderImage.png";
+import "swiper/css";
+import { useState } from "react";
+import cn from "classnames";
+
+const slides = [
+  {
+    header: "Как бизнесу сохранять IT-кадры на фоне кризиса",
+    paragraph:
+      "Инструменты, которые могут использовать компании для удержания сотрудников",
+    button: "Подробнее",
+    img: sliderImage,
+    id: 1,
+  },
+  {
+    header: "Как бизнесу сохранять IT-кадры на фоне кризиса",
+    paragraph:
+      "Инструменты, которые могут использовать компании для удержания сотрудников",
+    button: "Подробнее",
+    img: sliderImage,
+    id: 2,
+  },
+  {
+    header: "Как бизнесу сохранять IT-кадры на фоне кризиса",
+    paragraph:
+      "Инструменты, которые могут использовать компании для удержания сотрудников",
+    button: "Подробнее",
+    img: sliderImage,
+    id: 3,
+  },
+];
 
 export const Slider = () => {
+  const [active, setActive] = useState(0);
+  const swiperRef = useRef();
+
+  const isActive = (i, active) => {
+    return i === active
+      ? cn(
+          styles["swiper-pagination-bullet"],
+          styles["swiper-pagination-bullet-active"]
+        )
+      : styles["swiper-pagination-bullet"];
+  };
+
   return (
-    <section className={styles.slider}>
-      <Swiper
-        modules={[Pagination]}
-        pagination={{ clickable: true }}
-        className={styles.slider__swiper}
-        loop={true}
-      >
-        <SwiperSlide>
-          <div className={styles.slider__slide}>
-            <div className={styles.slide__text}>
-              <h1 className={styles.text__header}>
-                Как бизнесу сохранять <br />
-                IT-кадры на фоне кризиса
-              </h1>
-              <p className={styles.text__paragraph}>
-                Инструменты, которые могут использовать компании <br />
-                для удержания сотрудников
-              </p>
-              <button className={styles.text__button}>Подробнее</button>
-            </div>
-            <img src={sliderImage} alt="User" className={styles.slide__image} />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={styles.slider__slide}>
-            <div className={styles.slide__text}>
-              <h1 className={styles.text__header}>
-                Как бизнесу сохранять <br />
-                IT-кадры на фоне кризиса
-              </h1>
-              <p className={styles.text__paragraph}>
-                Инструменты, которые могут использовать компании <br />
-                для удержания сотрудников
-              </p>
-              <button className={styles.text__button}>Подробнее</button>
-            </div>
-            <img src={sliderImage} alt="User" className={styles.slide__image} />
-          </div>
-        </SwiperSlide>
-        <SwiperSlide>
-          <div className={styles.slider__slide}>
-            <div className={styles.slide__text}>
-              <h1 className={styles.text__header}>
-                Как бизнесу сохранять <br />
-                IT-кадры на фоне кризиса
-              </h1>
-              <p className={styles.text__paragraph}>
-                Инструменты, которые могут использовать компании <br />
-                для удержания сотрудников
-              </p>
-              <button className={styles.text__button}>Подробнее</button>
-            </div>
-            <img src={sliderImage} alt="User" className={styles.slide__image} />
-          </div>
-        </SwiperSlide>
-      </Swiper>
-    </section>
+    <>
+      <section className={styles.slider}>
+        <Swiper
+          onSlideChange={(swiper) => setActive(swiper.realIndex)}
+          onSwiper={(swiper) => (swiperRef.current = swiper)}
+          loop={true}
+        >
+          {slides.map((slide) => (
+            <SwiperSlide key={slide.id}>
+              <div className={styles.slider__slide}>
+                <div className={styles.slide__text}>
+                  <h1 className={styles.text__header}>{slide.header}</h1>
+                  <p className={styles.text__paragraph}>{slide.paragraph}</p>
+                  <button className={styles.text__button}>
+                    {slide.button}
+                  </button>
+                  <div className={styles.pagination__wrap}>
+                    {slides.map((_, i) => (
+                      <div
+                        onClick={() => swiperRef.current.slideTo(i++)}
+                        className={isActive(i++, active)}
+                      ></div>
+                    ))}
+                  </div>
+                </div>
+                <img
+                  src={sliderImage}
+                  alt="User"
+                  className={styles.slide__image}
+                />
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </section>
+    </>
   );
 };
