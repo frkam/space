d5423b44a604dc533c64edb1ed96b765e6c0beb4
frkam <gearboxop@gmail.com>
2022-06-24 06:46:24 +0300

    added line clampin` for acticleCard paragraphs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
